a8124658801e256700a1adc336c6bff12c45d499
nurfika03 <nurfika0511@gmail.com>
2024-12-30 15:11:14 +0900

    perbaikan

--- flutter_app_mobile/lib/main.dart
+++ flutter_app_mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_mobile/HomeAdmin.dart';
+// import 'package:flutter_app_mobile/HomePage.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'App Futsal',
-      home: AdminHomePage(),
+      home: AdminHomePage(), // Tetapkan salah satu halaman sebagai home
     );
   }
 }
